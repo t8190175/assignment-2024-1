cbbc427e65e7b23b6d09af1e7fd31317405c873f
t8190175 <104002503+t8190175@users.noreply.github.com>
2024-04-24 23:22:04 +0300

    Update tromino_tiling.py
    added print tromino( )
    
    change data structure for tromino array (list of lists)

--- tromino_tiling.py
+++ tromino_tiling.py
@@ -1,5 +1,47 @@
 import sys
 
+def print_tromino(n,p):
+    for i in range(2 ** n):
+        for j in range(2 ** n):
+            if j < 2 ** n - 1:
+                print(p[i][j], end=' ')
+            else:
+                print(p[i][j], end='\n')
+
+
+def create_tromino(n, p):
+    if n==1:
+        p[0][0] = 'G'
+        p[0][1] = 'X'
+        p[1][0] = 'G'
+        p[1][1] = 'G'
+        return p
+    if n==2:
+
+        p[1][1] = 'G'
+        p[1][2] = 'G'
+        p[2][1] = 'G'
+
+        p[2][2] = 'B'
+        p[2][3] = 'B'
+        p[3][2] = 'B'
+
+        p[0][0] = 'B'
+        p[0][1] = 'B'
+        p[1][0] = 'B'
+
+        p[2][0] = 'R'
+        p[3][0] = 'R'
+        p[3][1] = 'R'
+
+        p[0][2] = 'R'
+        p[0][3] = 'R'
+        p[1][3] = 'R'
+
+        return p
+
+
+
 try:
     n = int(sys.argv[1])
 except:
@@ -10,50 +52,13 @@ if (n<1):
     print('Program must be run followed by integer argument n, where n > 1')
     exit()
 
+p=[]
+for i in range(2**n):
+    row = []
+    for j in range(2**n):
+        row.append('X')
+    p.append(row)
 
 
-
-p={}
-p[(1,1)] = 'B'
-p[(1,2)] = 'B'
-p[(1,3)] = 'R'
-p[(1,4)] = 'R'
-p[(2,1)] = 'B'
-p[(2,2)] = 'G'
-p[(2,3)] = 'G'
-p[(2,4)] = 'R'
-p[(3,1)] = 'R'
-p[(3,2)] = 'G'
-p[(3,3)] = 'B'
-p[(3,4)] = 'B'
-p[(4,1)] = 'R'
-p[(4,2)] = 'R'
-p[(4,3)] = 'B'
-p[(4,4)] = 'X'
-
-
-for k, v in p.items():
-    pos = k
-    r=k[0]
-    c=k[1]
-    if c!=4:
-        print(v, end=' ')
-    else:
-        print(v, end='\n')
-
-##B B R R
-##B G G R
-##R G B B
-##R R B X
-
-n=input('n=')
-def create_tromino(n):
-    p={}
-    if n==1:
-        p[(1,1)] = 'G'
-        p[(1,2)] = 'X'
-        p[(2,1)] = 'G'
-        p[(2,2)] = 'G'
-        return p
-    if n==2:
-        return
+p = create_tromino(n, p)
+print_tromino(n,p)
